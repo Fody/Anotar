f8161baf0e75cb016f2fb975081053937ca59501
Simon Cropp <simon.cropp@gmail.com>
2012-11-02 00:30:23 +1100

    fix missing file

--- Template/NLogTemplate.cs
+++ Template/NLogTemplate.cs
@@ -1,32 +1,32 @@
 ﻿using System;
-using NLog;
 
 public class NLogTemplate
 {
-    static Logger AnotarLogger;
+    static NLog.Logger Logger;
+
     static NLogTemplate()
     {
-       AnotarLogger =LogManager.GetCurrentClassLogger();
+        Logger = NLog.LogManager.GetCurrentClassLogger();
     }
     void StringMethod()
     {
         var theMessage = "TheMessage";
-        AnotarLogger.Debug( string.Concat("MyMethod", theMessage));
+        Logger.Debug(string.Concat("MyMethod", theMessage));
     }
     void FormatMethod()
     {
         var format = "Hey {0}";
-        var theMessage = string.Format(format, new object[] {1});
-        AnotarLogger.Debug( string.Concat("MyMethod", theMessage));
+        var theMessage = string.Format(format, new object[] { 1 });
+        Logger.Debug(string.Concat("MyMethod", theMessage));
     }
     void ExceptionMethod()
     {
         var theMessage = "TheMessage";
         var exception = new Exception();
-        AnotarLogger.Debug(string.Concat("MyMethod", theMessage), exception);
+        Logger.Debug(string.Concat("MyMethod", theMessage), exception);
     }
     void SimpleMethod()
     {
-        AnotarLogger.Debug("MyMethod");
+        Logger.Debug("MyMethod");
     }
 }
