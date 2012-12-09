3cdb2f5121025d6da709ae9d9d91f722a118febb
Simon Cropp <simon.cropp@gmail.com>
2012-12-09 13:06:12 +1100

    ...

--- AssemblyToProcess/OnException.cs
+++ AssemblyToProcess/OnException.cs
@@ -9,6 +9,32 @@ public class OnException
     {
         throw new Exception("Foo");
     }
+    [LogToErrorOnException]
+    public void WithRefs(
+        ref string param1,
+        ref int param2,
+        ref short param3,
+        ref long param4,
+        ref uint param5,
+        ref ushort param6,
+        ref ulong param7,
+        ref bool param8,
+        ref double param9,
+        ref decimal param10,
+        ref int? param11,
+        ref object param12,
+        ref char param13,
+        ref DateTime param14,
+        ref Single param15,
+        ref IntPtr param16,
+        ref UInt16 param17,
+        ref UInt32 param18,
+        ref UInt64 param19,
+        ref UIntPtr param20
+        )
+    {
+        throw new Exception("Foo");
+    }
     [LogToInfoOnException]
     public void ToInfo(string param1, int param2)
     {
--- Fody/OnExceptionProcessor.cs
+++ Fody/OnExceptionProcessor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mono.Cecil;
@@ -23,6 +22,10 @@ class OnExceptionProcessor
 
     public MethodReference FormatMethod;
     VariableDefinition paramsArray;
+    StringBuilder messageBuilder;
+    int messageFormatIndex = 0;
+    VariableDefinition messageVariable;
+    VariableDefinition exceptionVariable;
 
     public void Process()
     {
@@ -88,11 +91,11 @@ class OnExceptionProcessor
 
     Instruction AddCatch()
     {
-        var exceptionVariable = new VariableDefinition(ExceptionReference);
+        exceptionVariable = new VariableDefinition(ExceptionReference);
         body.Variables.Add(exceptionVariable);
-        var messageVariable = new VariableDefinition(TypeSystem.String);
+        messageVariable = new VariableDefinition(TypeSystem.String);
         body.Variables.Add(messageVariable);
-         paramsArray = new VariableDefinition(new ArrayType(TypeSystem.Object));
+        paramsArray = new VariableDefinition(new ArrayType(TypeSystem.Object));
         body.Variables.Add(paramsArray);
         var startNop = Instruction.Create(OpCodes.Nop);
         body.Instructions.Add(startNop);
@@ -100,66 +103,196 @@ class OnExceptionProcessor
         body.Instructions.Add(Instruction.Create(OpCodes.Stloc, exceptionVariable));
 
 
-        var messageLdstr = Instruction.Create(OpCodes.Ldstr,"");
+        var messageLdstr = Instruction.Create(OpCodes.Ldstr, "");
         body.Instructions.Add(messageLdstr);
         body.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4, Method.Parameters.Count));
         body.Instructions.Add(Instruction.Create(OpCodes.Newarr, TypeSystem.Object));
         body.Instructions.Add(Instruction.Create(OpCodes.Stloc, paramsArray));
 
-        var stringBuilder = new StringBuilder(string.Format("Exception occurred in '{0}.{1}'. ", Method.DeclaringType.Name, Method.Name));
-        var paramsToLog = GetParamsToLog().ToList();
-        for (var index = 0; index < paramsToLog.Count; index++)
+        messageBuilder = new StringBuilder(string.Format("Exception occurred in '{0}.{1}'. ", Method.DeclaringType.Name, Method.Name));
+        foreach (var parameterDefinition in Method.Parameters)
         {
-            var parameterDefinition = Method.Parameters[index];
-
-            stringBuilder.AppendFormat(" {0} '{{{1}}}'", parameterDefinition.Name, index);
-            body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, paramsArray));
-            body.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4, index));
-            body.Instructions.Add(Instruction.Create(OpCodes.Ldarg, parameterDefinition));
-            
-            if (parameterDefinition.ParameterType.IsValueType)
-            {
-                body.Instructions.Add(Instruction.Create(OpCodes.Box, parameterDefinition.ParameterType));
-            }
-            body.Instructions.Add(Instruction.Create(OpCodes.Stelem_Ref));
+            ProcessParam(parameterDefinition);
         }
-        messageLdstr.Operand = stringBuilder.ToString();
+        messageLdstr.Operand = messageBuilder.ToString();
 
-            body.Instructions.Add(Instruction.Create(OpCodes.Ldloc,paramsArray));
+        body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, paramsArray));
         body.Instructions.Add(Instruction.Create(OpCodes.Call, FormatMethod));
         body.Instructions.Add(Instruction.Create(OpCodes.Stloc, messageVariable));
 
-        Instruction sectionNop = Instruction.Create(OpCodes.Nop);
-        body.Instructions.Add(Instruction.Create(OpCodes.Ldsfld, FieldDefinition));
-        body.Instructions.Add(Instruction.Create(OpCodes.Callvirt, Injector.IsDebugEnabledMethod));
-        body.Instructions.Add(Instruction.Create(OpCodes.Brfalse_S, sectionNop));
-        body.Instructions.Add(Instruction.Create(OpCodes.Ldsfld, FieldDefinition));
-        body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, messageVariable));
-        body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, exceptionVariable));
-        body.Instructions.Add(Instruction.Create(OpCodes.Callvirt, Injector.DebugExceptionMethod));
-        body.Instructions.Add(sectionNop);
+        if (foundDebug)
+        {
+            AddWrite(Injector.IsDebugEnabledMethod, Injector.DebugExceptionMethod);
+        }
+        if (foundInfo)
+        {
+            AddWrite(Injector.IsInfoEnabledMethod, Injector.InfoExceptionMethod);
+        }
+        if (foundWarn)
+        {
+            AddWrite(Injector.IsWarnEnabledMethod, Injector.WarnExceptionMethod);
+        }
+        if (foundError)
+        {
+            AddWrite(Injector.IsErrorEnabledMethod, Injector.ErrorExceptionMethod);
+        }
 
         body.Instructions.Add(Instruction.Create(OpCodes.Rethrow));
         return startNop;
     }
 
-    IEnumerable<ParameterDefinition> GetParamsToLog()
+    void AddWrite(MethodReference isEnabledMethod, MethodReference writeMethod)
     {
-        foreach (var parameterDefinition in Method.Parameters)
-        {
-            var paramMetaData = parameterDefinition.ParameterType.MetadataType;
-            if (paramMetaData == MetadataType.UIntPtr ||
-                paramMetaData == MetadataType.FunctionPointer ||
-                paramMetaData == MetadataType.IntPtr ||
-                paramMetaData == MetadataType.Pointer)
-            {
-                // We don't want to log values of these parameters, so skip
-                // this iteration
-                continue;
-            }
-            yield return  parameterDefinition;
-        }
+        var sectionNop = Instruction.Create(OpCodes.Nop);
+        body.Instructions.Add(Instruction.Create(OpCodes.Ldsfld, FieldDefinition));
+        body.Instructions.Add(Instruction.Create(OpCodes.Callvirt, isEnabledMethod));
+        body.Instructions.Add(Instruction.Create(OpCodes.Brfalse_S, sectionNop));
+        body.Instructions.Add(Instruction.Create(OpCodes.Ldsfld, FieldDefinition));
+        body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, messageVariable));
+        body.Instructions.Add(Instruction.Create(OpCodes.Ldloc, exceptionVariable));
+        body.Instructions.Add(Instruction.Create(OpCodes.Callvirt, writeMethod));
+        body.Instructions.Add(sectionNop);
     }
+
+    void ProcessParam(ParameterDefinition parameterDefinition)
+    {
+
+        var paramMetaData = parameterDefinition.ParameterType.MetadataType;
+        if (paramMetaData == MetadataType.UIntPtr ||
+            paramMetaData == MetadataType.FunctionPointer ||
+            paramMetaData == MetadataType.IntPtr ||
+            paramMetaData == MetadataType.Pointer)
+        {
+            return;
+        }
+        var instructions = body.Instructions;
+        instructions.Add(Instruction.Create(OpCodes.Ldloc, paramsArray));
+        instructions.Add(Instruction.Create(OpCodes.Ldc_I4, messageFormatIndex));
+        instructions.Add(Instruction.Create(OpCodes.Ldarg, parameterDefinition));
+
+
+        // Reset boolean flag variable to false
+
+        // If aparameter is passed by reference then you need to use ldind
+        // ------------------------------------------------------------
+        var paramType = parameterDefinition.ParameterType;
+        if (paramType.IsByReference)
+        {
+            var referencedTypeSpec = (TypeSpecification) paramType;
+
+            var pointerToValueTypeVariable = false;
+            switch (referencedTypeSpec.ElementType.MetadataType)
+            {
+                    //Indirect load value of type int8 as int32 on the stack
+                case MetadataType.Boolean:
+                case MetadataType.SByte:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_I1));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type int16 as int32 on the stack
+                case MetadataType.Int16:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_I2));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type int32 as int32 on the stack
+                case MetadataType.Int32:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_I4));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type int64 as int64 on the stack
+                    // Indirect load value of type unsigned int64 as int64 on the stack (alias for ldind.i8)
+                case MetadataType.Int64:
+                case MetadataType.UInt64:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_I8));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type unsigned int8 as int32 on the stack
+                case MetadataType.Byte:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_U1));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type unsigned int16 as int32 on the stack
+                case MetadataType.UInt16:
+                case MetadataType.Char:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_U2));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type unsigned int32 as int32 on the stack
+                case MetadataType.UInt32:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_U4));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type float32 as F on the stack
+                case MetadataType.Single:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_R4));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type float64 as F on the stack
+                case MetadataType.Double:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_R8));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                    // Indirect load value of type native int as native int on the stack
+                case MetadataType.IntPtr:
+                case MetadataType.UIntPtr:
+                    instructions.Add(Instruction.Create(OpCodes.Ldind_I));
+                    pointerToValueTypeVariable = true;
+                    break;
+
+                default:
+                    // Need to check if it is a value type instance, in which case
+                    // we use ldobj instruction to copy the contents of value type
+                    // instance to stack and then box it
+                    if (referencedTypeSpec.ElementType.IsValueType)
+                    {
+                        instructions.Add(Instruction.Create(OpCodes.Ldobj, referencedTypeSpec.ElementType));
+                        pointerToValueTypeVariable = true;
+                    }
+                    else
+                    {
+                        // It is a reference type so just use reference the pointer
+                        instructions.Add(Instruction.Create(OpCodes.Ldind_Ref));
+                    }
+                    break;
+            }
+
+            if (pointerToValueTypeVariable)
+            {
+                // Box the dereferenced parameter type
+                instructions.Add(Instruction.Create(OpCodes.Box, referencedTypeSpec.ElementType));
+            }
+
+        }
+        else
+        {
+
+            // If it is a value type then you need to box the instance as we are going 
+            // to add it to an array which is of type object (reference type)
+            // ------------------------------------------------------------
+            if (paramType.IsValueType)
+            {
+                // Box the parameter type
+                instructions.Add(Instruction.Create(OpCodes.Box, paramType));
+            }
+        }
+
+        // Store parameter in object[] array
+        // ------------------------------------------------------------
+        instructions.Add(Instruction.Create(OpCodes.Stelem_Ref));
+        messageBuilder.AppendFormat(" {0} '{{{1}}}'", parameterDefinition.Name, messageFormatIndex);
+
+        messageFormatIndex++;
+    }
+
 
     Instruction FirstInstructionSkipCtor()
     {
@@ -170,5 +303,4 @@ class OnExceptionProcessor
         return body.Instructions.First();
     }
 
-    
 }
