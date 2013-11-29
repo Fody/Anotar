802c7b10c6a6f213a592c6777d941869f9c2152e
Justin Thirkell <justin.thirkell@xero.com>
2013-11-29 14:31:06 +1300

    hacked the version number and nuspec

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Anotar")]
 [assembly: AssemblyProduct("Anotar")]
-[assembly: AssemblyVersion("2.7.0")]
-[assembly: AssemblyFileVersion("2.7.0")]
+[assembly: AssemblyVersion("2.6.9")]
+[assembly: AssemblyFileVersion("2.6.9")]
