5cd9822d887a7455ec5bad1bf789b400ee621358
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 10:59:12 +1100

    move over to dev dependency

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Anotar")]
 [assembly: AssemblyProduct("Anotar")]
-[assembly: AssemblyVersion("2.8.0")]
-[assembly: AssemblyFileVersion("2.8.0")]
+[assembly: AssemblyVersion("2.8.1")]
+[assembly: AssemblyFileVersion("2.8.1")]
