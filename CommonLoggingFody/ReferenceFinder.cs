18a7df79082dfa4e68909dab1b45670332949653
Simon Cropp <simon.cropp@gmail.com>
2014-05-28 12:44:32 +1000

    remove dependency on common

--- CommonLoggingFody/ReferenceFinder.cs
+++ CommonLoggingFody/ReferenceFinder.cs
@@ -4,30 +4,12 @@ using Mono.Cecil;
 
 public partial class ModuleWeaver
 {
-	public AssemblyDefinition CommonLoggingReference;
 	public Lazy<AssemblyDefinition> CommonLoggingCoreReference;
 
 	void FindReference()
 	{
-        CommonLoggingReference = GetCommonLogging();
         CommonLoggingCoreReference = new Lazy<AssemblyDefinition>(GetCommonCoreLogging);
 	}
-
-    AssemblyDefinition GetCommonLogging()
-    {
-        var existingReference = ModuleDefinition.AssemblyReferences.FirstOrDefault(x => x.Name == "Common.Logging");
-
-        if (existingReference != null)
-        {
-            return AssemblyResolver.Resolve(existingReference);
-        }
-        var reference = AssemblyResolver.Resolve("Common.Logging");
-        if (reference != null)
-        {
-            return reference;
-        }
-        throw new Exception("Could not resolve a reference to Common.Logging.dll.");
-    }
 
     AssemblyDefinition GetCommonCoreLogging()
     {
