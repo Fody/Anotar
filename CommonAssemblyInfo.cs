8ceb08305679e8c17617ea6067d64ecf3ac8ca6d
Simon Cropp <simon.cropp@gmail.com>
2014-06-27 12:33:43 +0100

    refs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Anotar")]
 [assembly: AssemblyProduct("Anotar")]
-[assembly: AssemblyVersion("2.10.2")]
-[assembly: AssemblyFileVersion("2.10.2")]
+[assembly: AssemblyVersion("2.11.0")]
+[assembly: AssemblyFileVersion("2.11.0")]
