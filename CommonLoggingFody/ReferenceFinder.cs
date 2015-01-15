8c262f30a60c617637c41f9727c581d565dc4b17
Tim Murphy <tim@26tp.com>
2015-01-16 05:30:39 +1100

    Updated Anotar.CommonLogging.Fody to use Common.Logging 3.0.0.
    - Updated Common.Logging NuGet package to 3.0.0.
    - Updated Anotor.CommonLogging.nuspec's Common.Logging dependency to 3.0.0.
    - Fixed code to handle ILog moving from Common.Logging to Common.Logging.Core.

--- CommonLoggingFody/ReferenceFinder.cs
+++ CommonLoggingFody/ReferenceFinder.cs
@@ -5,25 +5,27 @@ using Mono.Cecil;
 public partial class ModuleWeaver
 {
 	public Lazy<AssemblyDefinition> CommonLoggingReference;
+    public Lazy<AssemblyDefinition> CommonLoggingCoreReference;
 
 	void FindReference()
 	{
-        CommonLoggingReference = new Lazy<AssemblyDefinition>(GetCommonLogging);
+        CommonLoggingReference = new Lazy<AssemblyDefinition>(() => GetReference("Common.Logging"));
+        CommonLoggingCoreReference = new Lazy<AssemblyDefinition>(() => GetReference("Common.Logging.Core"));
 	}
 
-    AssemblyDefinition GetCommonLogging()
+    AssemblyDefinition GetReference(string referenceName)
     {
-        var existingReference = ModuleDefinition.AssemblyReferences.FirstOrDefault(x => x.Name == "Common.Logging");
+        var existingReference = ModuleDefinition.AssemblyReferences.FirstOrDefault(x => x.Name == referenceName);
 
         if (existingReference != null)
         {
             return AssemblyResolver.Resolve(existingReference);
         }
-        var reference = AssemblyResolver.Resolve("Common.Logging");
+        var reference = AssemblyResolver.Resolve(referenceName);
         if (reference != null)
         {
             return reference;
         }
-        throw new Exception("Could not resolve a reference to Common.Logging.dll.");
+        throw new Exception(string.Format("Could not resolve a reference to {0}.dll.", referenceName));
     }
 }
