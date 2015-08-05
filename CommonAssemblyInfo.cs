a9ee0e7f70040825ed62b9254904efebeca7abb4
Simon Cropp <simon.cropp@gmail.com>
2015-08-05 16:28:32 +1000

    fix xdt import

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Anotar")]
 [assembly: AssemblyProduct("Anotar")]
-[assembly: AssemblyVersion("2.18.2")]
-[assembly: AssemblyFileVersion("2.18.2")]
+[assembly: AssemblyVersion("2.18.3")]
+[assembly: AssemblyFileVersion("2.18.3")]
