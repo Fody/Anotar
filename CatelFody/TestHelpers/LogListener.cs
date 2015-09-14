8d2bbfbc4dbd9593e215340c5c90fd5ef5454b16
Simon Cropp <simon.cropp@gmail.com>
2015-09-15 08:32:13 +1000

    bump fody and common logging

--- CatelFody/TestHelpers/LogListener.cs
+++ CatelFody/TestHelpers/LogListener.cs
@@ -6,8 +6,8 @@ using Scalpel;
 public class LogListener : LogListenerBase
 {
     public Action<string,LogEvent> Action;
-    
-    protected override void Write(ILog log, string message, LogEvent logEvent, object extraData, DateTime time)
+
+    protected override void Write(ILog log, string message, LogEvent logEvent, object extraData, LogData logData, DateTime time)
     {
         Action(message, logEvent);
     }
