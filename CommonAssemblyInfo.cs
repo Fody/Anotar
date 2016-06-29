97c3a84ed7b426797646e971f0e01e27464ccfdc
Simon Cropp <simon.cropp@gmail.com>
2016-06-29 12:10:45 +1000

    bump serilog

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Anotar")]
 [assembly: AssemblyProduct("Anotar")]
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
