65fa2f9327a10e85d3cc774ea1900797cee0b103
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2017-06-04 21:12:33 +0200

    adding integration tests for Custom

---
+++ IntegrationTests/AnotarCustom.cs
@@ -0,0 +1,14 @@
+﻿[assembly: Anotar.Custom.LoggerFactory(typeof(LoggerFactory))]
+
+namespace IntegrationTests
+{
+    public class AnotarCustom
+    {
+        public void Weave()
+        {
+            Anotar.Custom.LogTo.Debug("This weaver should work: {0}", "Custom");
+        }
+    }
+}
+
+
