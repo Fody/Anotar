e4a52858baf17cdf49e320b926517d86037037c1
tpluscode <tpluscode@users.noreply.github.com>
2017-09-01 07:39:46 +0200

    update FodyCecil

--- CustomAssemblyToProcess/AssemblyInfo.cs
+++ CustomAssemblyToProcess/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
 using Anotar.Custom;
 
 [assembly: LoggerFactory(typeof(LoggerFactory))]
+
+[assembly: AssemblyVersion("3.3.1.0")]
+[assembly: AssemblyFileVersion("3.3.1.0")]
+[assembly: AssemblyInformationalVersion("3.3.1-catel5-log-exception.1+41.Branch.catel5-log-exception.Sha.6efe4d3a3f017095dc2b64df84c24a724f7da351")]
--- CustomReferenceAssembly/AssemblyInfo.cs
+++ CustomReferenceAssembly/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.3.1.0")]
+[assembly: AssemblyFileVersion("3.3.1.0")]
+
+[assembly: AssemblyInformationalVersion("3.3.1-catel5-log-exception.1+41.Branch.catel5-log-exception.Sha.6efe4d3a3f017095dc2b64df84c24a724f7da351")]
